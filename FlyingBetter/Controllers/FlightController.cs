850d5fa5999986cb2116994e3bf9052baba4ebad
MarcinBieganek <marcin.bieganek@gmail.com>
2022-01-10 20:44:53 +0100

    Basic ASP.NET MVC app created with search form

---
+++ FlyingBetter/Controllers/FlightController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FlyingBetter.Models.FlightSearch;
+
+namespace FlyingBetter.Controllers
+{
+    public class FlightController : Controller
+    {
+        [HttpGet]
+        public ActionResult Search()
+        {
+            var model = new FlightSearchModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Search(FlightSearchModel model)
+        {
+            if (this.ModelState.IsValid)
+            {
+                return RedirectToAction("Result", model);
+            }
+ 
+            return View(model);
+        }
+
+        public ActionResult Result(FlightSearchModel model)
+        {
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FlyingBetter/Models/FlightSearch/FlightSearchModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlyingBetter.Models.FlightSearch
+{
+    public class FlightSearchModel
+    {
+        [Required(ErrorMessage = "Location From is required.")]
+        [MinLength(2, ErrorMessage = "Too short.")]
+        [MaxLength(50, ErrorMessage = "Too long.")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]+$", ErrorMessage = "Only letters")]
+        public string From { get; set; }
+
+        [Required(ErrorMessage = "Location To is required.")]
+        [MinLength(2, ErrorMessage = "Too short.")]
+        [MaxLength(50, ErrorMessage = "Too long.")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]+$", ErrorMessage = "Only letters")]
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Flight date is required")]
+        [DataType(DataType.Date, ErrorMessage = "Format is in incorrect format.")]
+        public DateTime Date { get; set; }
+    }
+}
